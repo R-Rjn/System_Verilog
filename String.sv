9cb393126d0f0de7a0fc5b69f0937c73790a7780
RAVI RANJAN <77184885+R-Rjn@users.noreply.github.com>
2023-06-14 18:29:07 +0530

    Update String.sv

--- String.sv
+++ String.sv
@@ -5,12 +5,9 @@ initial
  begin
 A = "RAM ";
 B = "SHAYAM";
-   $display(" %d ",A.len() ); //printing length of string A.
+$display(" %d ",A.len() ); //printing length of string A.
 $display(" %s ",A.getc(5) );
 $display(" %s ",A.tolower);
 $display(" %s ",B.toupper);
-$display(" %d ",B.compare(A) );
-$display(" %d ",A.compare("test") );
-
 end
 endmodule
